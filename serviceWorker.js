4208975ca8caaea46249d1ce2d812bcf3a1d0a4a
fkrait <fkrait@hotmail.com>
2025-12-28 20:00:09 +0100

    Update service worker cache version and assets
    Updated cache version and added new assets to cache.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,17 +1,31 @@
-const CACHE_NAME = 'taekwondo-reaction-v1';
+const CACHE_NAME = 'taekwondo-reaction-v2';
 const urlsToCache = [
   '/',
   'index.html',
   'style.css',
   'main.js',
-  'icon-512.png',
   'manifest.json',
+  'icon-512.png',
+  'icon-192.png',
+  'attached_assets/gradient_background.PNG',
   'https://www.soundjay.com/human/applause-01.mp3'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name.startsWith('taekwondo-reaction-') && name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
   );
 });
 
